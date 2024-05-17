5cb2e1bacf6a225a85d5b367375a676050022879
hansi-thenuwara <hansi.thenuwara@gmail.com>
2024-05-17 17:34:11 +0530

    ecommerce:legacy:product crud operations

--- src/main/java/com/ecommerce/legacy/controller/ProductController.java
+++ src/main/java/com/ecommerce/legacy/controller/ProductController.java
@@ -1,13 +1,39 @@
 package com.ecommerce.legacy.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ecommerce.legacy.entity.Product;
+import com.ecommerce.legacy.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/products")
 public class ProductController {
 
-    @GetMapping("/products")
-    public String getProducts(){
-        return "List of Products";
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping
+    public List<Product> getProducts(){
+        return productRepository.findAll();
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product){
+        return productRepository.save(product);
+    }
+
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable Long id, @RequestBody Product productDetails){
+        Product product = productRepository.findById(id).orElseThrow();
+        product.setName(productDetails.getName());
+        product.setPrice(productDetails.getPrice());
+        return productRepository.save(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Long id){
+        productRepository.deleteById(id);
     }
 }
