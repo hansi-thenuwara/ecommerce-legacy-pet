e08bc23f2be8a8e73fb346fff91646a68f36c35f
hansi-thenuwara <hansi.thenuwara@gmail.com>
2024-05-17 17:04:45 +0530

    ecommerce:legacy:initial commit

---
+++ src/main/java/com/ecommerce/legacy/controller/ProductController.java
@@ -0,0 +1,13 @@
+package com.ecommerce.legacy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    @GetMapping("/products")
+    public String getProducts(){
+        return "List of Products";
+    }
+}
